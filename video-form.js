7bb889104a0bb5888fe76a1fc977e696f6ac5703
Christoph Nachreiner <christoph.nachreiner@gmail.com>
2018-01-23 17:19:30 +0100

    added initial form overlay

---
+++ video-form.js
@@ -0,0 +1,31 @@
+class VideoForm {
+    constructor(formOverlay) {
+        this.overlay = formOverlay;
+        this.form = formOverlay.querySelector("#add-video")
+    }
+
+    getFormData( form ) {
+        var obj = {};
+        var elements = form.querySelectorAll( "input, select, textarea" );
+        for( var i = 0; i < elements.length; ++i ) {
+            var element = elements[i];
+            var name = element.name;
+            var value = element.value;
+
+            if( name ) {
+                obj[ name ] = value;
+            }
+        }
+
+        return obj;
+    }
+
+    open(){ 
+        this.overlay.classList.add('visible');
+    }
+
+    close(){
+        this.overlay.classList.remove('visible')
+    }
+}
+
